eda96eb944f84ab5df87fd352ed9f26c25d7ad59
Yevhenii Medviediev <medvedev.gb@gmail.com>
2023-05-25 04:23:05 +0300

    fix(): play button

--- server/src/desktop-controller/action-resolvers/play.py
+++ server/src/desktop-controller/action-resolvers/play.py
@@ -1,3 +1,12 @@
 import pyautogui
 
-playButton = pyautogui.click('./elements/play_button.png')
+# Get the screen size
+screen_width, screen_height = pyautogui.size()
+
+# Calculate the center coordinates
+center_x = screen_width // 2
+center_y = screen_height // 2
+
+# Move the mouse to the center of the screen and click
+pyautogui.moveTo(center_x, center_y)
+pyautogui.click()
