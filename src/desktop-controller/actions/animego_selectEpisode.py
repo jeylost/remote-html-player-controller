83d45e7f29f6ad48321d9131ed4a9c96c5c380a7
Yevhenii Medviediev <medvedev.gb@gmail.com>
2023-05-26 21:17:41 +0300

    fix(): episode selection for animego

--- src/desktop-controller/actions/animego_selectEpisode.py
+++ src/desktop-controller/actions/animego_selectEpisode.py
@@ -3,7 +3,11 @@ import sys
 
 arguments = sys.argv
 
-pyautogui.press(['esc'])
-pyautogui.press(['tab'])
+episodeSelectorLabel = pyautogui.locateOnScreen('./screenshots/animego_selectEpisode.png')
+episodeSelectorLabelPoint = pyautogui.center(episodeSelectorLabel)
+episodeSelectorLabelPointX, episodeSelectorLabelPointY = button7point
+
+pyautogui.click(episodeSelectorLabelPointX + 20, episodeSelectorLabelPointY)
+
 pyautogui.write(arguments[1])
 pyautogui.press(['enter'])
